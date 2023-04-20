37148efcda92e9248c2f65652daba939e26b91ac
Eliel Martins <elieuprata@gmail.com>
2023-04-20 17:52:45 -0300

    concluded SignIn Screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,18 +1,45 @@
-import { Container } from './styles'
+import { Button } from '@components/Button'
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+} from './styles'
 
 import { Input } from '@components/Input'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+
+import BrandImg from '@assets/brand.png'
 
 export function SignIn() {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+          <Brand source={BrandImg} />
 
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <Title>Login</Title>
+
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
