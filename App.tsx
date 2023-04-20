ea5084a30400b5608fdfcec4e322ba34f23933e1
Eliel Martins <elieuprata@gmail.com>
2023-04-20 15:32:49 -0300

    install and configure firebase and styled components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
-import { View } from 'react-native'
 import AppLoading from 'expo-app-loading'
 
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans'
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display'
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from '@src/theme'
+
+import { SignIn } from '@src/screens/SignIn'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,5 +18,9 @@ export default function App() {
     return <AppLoading />
   }
 
-  return <View></View>
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  )
 }
