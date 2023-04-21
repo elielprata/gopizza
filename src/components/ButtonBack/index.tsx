b440a4e48a604a01b8b4e9496337697948582840
Eliel Martins <elieuprata@gmail.com>
2023-04-21 15:32:55 -0300

    create Button back component an Photo component

---
+++ src/components/ButtonBack/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styles'
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacityProps } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
+export function ButtonBack({ ...rest }: TouchableOpacityProps) {
+  const { COLORS } = useTheme()
+  return (
+    <Container>
+      <MaterialIcons name="chevron-left" size={18} color={COLORS.TITLE} />
+    </Container>
+  )
+}
--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -2,15 +2,23 @@ import { Platform } from 'react-native'
 import { Container, DeleteLabel, Header, Title } from './styles'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 
+import { ButtonBack } from '@components/ButtonBack'
+import { Photo } from '@components/Photo'
+
 export function Product() {
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
+        <ButtonBack />
+
         <Title>Cadastrar</Title>
+
         <TouchableOpacity>
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
+
+      <Photo uri="" />
     </Container>
   )
 }
