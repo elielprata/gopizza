99e0373c63c4a098bef07b58dee394e9b9b58cc1
Eliel Martins <elieuprata@gmail.com>
2023-04-21 18:10:20 -0300

    use Image Picker dependency to load image

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -8,11 +8,30 @@ import {
   Upload,
 } from './styles'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import * as ImagePicker from 'expo-image-picker'
 
 import { ButtonBack } from '@components/ButtonBack'
 import { Photo } from '@components/Photo'
+import { useState } from 'react'
 
 export function Product() {
+  const [image, setImage] = useState('')
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+      })
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri)
+      }
+    }
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -26,9 +45,13 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri="" />
+        <Photo uri={image} />
 
-        <PickImageButton title="Carregar" type="secondary" />
+        <PickImageButton
+          title="Carregar"
+          type="secondary"
+          onPress={handlePickerImage}
+        />
       </Upload>
     </Container>
   )
