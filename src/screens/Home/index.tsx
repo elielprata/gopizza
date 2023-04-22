f1732c1703639782a51c833eb05bb88ab8e63fed
Eliel Martins <elieuprata@gmail.com>
2023-04-22 08:57:18 -0300

    list pizzas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,13 +11,53 @@ import {
 import { MaterialIcons } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
 import { useTheme } from 'styled-components/native'
+import {
+  collection,
+  endAt,
+  getDocs,
+  orderBy,
+  query,
+  startAt,
+} from 'firebase/firestore'
+import { firestore } from '../../../firebaseConfig'
 
 import HappyEmojiPng from '../../assets/happy.png'
+
 import { Search } from '@components/Search'
-import { ProductCard } from '@components/ProductCard'
+import { ProductCard, ProductProps } from '@components/ProductCard'
+import { useEffect, useState } from 'react'
+import { FlatList } from 'react-native-gesture-handler'
 
 export function Home() {
+  const [pizzas, setPizzas] = useState<ProductProps[]>([])
+
   const { COLORS } = useTheme()
+
+  async function fetchPizzas(value: string) {
+    const formatedValue = value.toLowerCase().trim()
+
+    const q = query(
+      collection(firestore, 'pizzas'),
+      orderBy('name_insensitive'),
+      startAt(formatedValue),
+      endAt(`${formatedValue}\uf8ff`)
+    )
+
+    const data = await getDocs(q)
+
+    const products = data.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      }
+    }) as ProductProps[]
+
+    setPizzas(products)
+  }
+
+  useEffect(() => {
+    fetchPizzas('')
+  }, [])
 
   return (
     <Container>
@@ -39,12 +79,15 @@ export function Home() {
         <MenuItemsNumber>10 pizzas</MenuItemsNumber>
       </MenuHeader>
 
-      <ProductCard
-        data={{
-          photo_url: 'https://github.com/elielprata.png',
-          name: 'Marguerita',
-          description: 'asasdf',
-          id: 'aasdf',
+      <FlatList
+        data={pizzas}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ProductCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingTop: 20,
+          paddingBottom: 125,
+          marginHorizontal: 24,
         }}
       />
     </Container>
