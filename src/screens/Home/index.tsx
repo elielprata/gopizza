9148e42d4e1b25dd45b956fe29400f2fdbd5ea95
Eliel Martins <elieuprata@gmail.com>
2023-04-22 06:48:46 -0300

    create Header of Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,31 @@
-import { Container } from './styles'
+import {
+  Container,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+  Header,
+} from './styles'
+import { MaterialIcons } from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native'
+import { useTheme } from 'styled-components/native'
+
+import HappyEmojiPng from '../../assets/happy.png'
 
 export function Home() {
-  return <Container></Container>
+  const { COLORS } = useTheme()
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={HappyEmojiPng} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+        </TouchableOpacity>
+      </Header>
+    </Container>
+  )
 }
