2879ed0f4dc449e3ab581294517057c1a8b93a6c
Eliel Martins <elieuprata@gmail.com>
2023-04-24 08:44:41 -0300

    sending Order to firestore

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -23,9 +23,10 @@ import { PIZZA_TYPES } from '@utils/pizzaTypes'
 import { Button } from '@components/Button'
 
 import { OrderNavigationProps } from 'src/@types/navigation'
-import { doc, getDoc } from 'firebase/firestore'
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
 import { firestore } from '../../../firebaseConfig'
 import { ProductProps } from '@components/ProductCard'
+import { useAuth } from '@hooks/auth'
 
 type PizzaResponse = ProductProps & {
   price_sizes: {
@@ -36,13 +37,53 @@ type PizzaResponse = ProductProps & {
 export function Order() {
   const [size, setSize] = useState('')
   const [pizza, setPizza] = useState<PizzaResponse>({} as PizzaResponse)
+  const [quantity, setQuantity] = useState(1)
+  const [tableNumber, setTableNumber] = useState('')
+  const [sendingOrder, setSendingOrder] = useState(false)
 
+  const { user } = useAuth()
   const navigation = useNavigation()
   const route = useRoute()
   const { id } = route.params as OrderNavigationProps
 
+  const amount = size ? pizza.price_sizes[size] * quantity : '0.00'
+
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  async function handleOrder() {
+    if (!size) {
+      return Alert.alert('Pedido', 'Selecione o tamanho da pizza.')
+    }
+
+    if (!tableNumber) {
+      return Alert.alert('Pedido', 'Informe o número da mesa.')
+    }
+
+    if (!quantity) {
+      return Alert.alert('Pedido', 'Informe a quantidade.')
+    }
+
+    setSendingOrder(true)
+
+    try {
+      await setDoc(doc(collection(firestore, 'orders')), {
+        quantity,
+        amount,
+        pizza: pizza.name,
+        size,
+        table_number: tableNumber,
+        status: 'Preparando',
+        waiter_id: user?.id,
+        image: pizza.photo_url,
+      })
+
+      navigation.navigate('home')
+    } catch (error) {
+      Alert.alert('Pedido', 'Não foi possível realizar o pedido.')
+      setSendingOrder(false)
+    }
   }
 
   async function getPizzaData() {
@@ -85,19 +126,27 @@ export function Order() {
             <InputGroup>
               <Label>Número da mesa</Label>
 
-              <Input keyboardType="numeric" />
+              <Input keyboardType="numeric" onChangeText={setTableNumber} />
             </InputGroup>
 
             <InputGroup>
               <Label>Quantidade</Label>
 
-              <Input keyboardType="numeric" />
+              <Input
+                keyboardType="numeric"
+                value={quantity.toString()}
+                onChangeText={(value) => setQuantity(Number(value))}
+              />
             </InputGroup>
           </FormRow>
 
-          <Price>Valor de R$ 50.00</Price>
+          <Price>Valor de R$ {amount}</Price>
 
-          <Button title="Confirmar pedido" />
+          <Button
+            title="Confirmar pedido"
+            onPress={handleOrder}
+            isLoading={sendingOrder}
+          />
         </Form>
       </ContentScroll>
     </Container>
